b732557e281a2ad5621b1d86393770c8263b338c
ManuelSfilho <52848138+ManuelSfilho@users.noreply.github.com>
2026-06-11 17:08:17 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,42 @@
-const CACHE = "granel-v1";
+const CACHE = "grafica-personalizada-v1";
+
+const ARQUIVOS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
+
   event.waitUntil(
-    caches.open(CACHE).then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json",
-        "./icon-192.png",
-        "./icon-512.png"
-      ]);
-    })
+    caches.open(CACHE)
+      .then(cache => cache.addAll(ARQUIVOS))
   );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+      .catch(() => caches.match("./index.html"))
   );
 });
